ca524396e5062687ba1377a775aee1b23f769a3c
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-07 14:30:05 +0530

    certificate complete

--- components/certificate.tsx
+++ components/certificate.tsx
@@ -1,25 +1,81 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Certificate() {
   return (
-   <section>
-    <h1>Certificate</h1>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="certificate">
-            <div className="certificate-header">
-              <h2>ESG Certificate</h2>
+   <section className=''>
+      <h1 className='text-2xl font-semibold ml-5 mb-5'>
+        See if your company has a popular ESG Certificate
+      </h1>
+      <div className='grid lg:grid-cols-3 gap-x-5 gap-y-10 items-center justify-between'>
+        {certificates.map((certificate, index) => (
+          <Link key={index} className='card' href={certificate.link}>
+            <div className='flex flex-col items-start justify-center rounded-lg bg-green-200 p-5'>
+              <Image src={certificate.image} alt={certificate.title} width={200} height={0} className='w-full overflow-clip'/>
+              <h2 className='text-sm'>{certificate.title}</h2>
             </div>
-            <div className="certificate-body">
-              <p>This is to certify that the ESG rating of the company is 4.5 out of 5.0.</p>
-            </div>
-          </div>
-        </div>
+              
+          </Link>
+        ))}
       </div>
-    </div>
    </section>
   )
 }
 
 export default Certificate
+
+
+
+const certificates = [
+  {
+    image: '/certificates/certificate-1.png',
+    title: 'Certificate 1',
+    link: '/certificate-1'
+  },
+  {
+    image: '/certificates/certificate-2.png',
+    title: 'Certificate 2',
+    link: '/certificate-2'
+  },
+  {
+    image: '/certificates/certificate-3.png',
+    title: 'Certificate 3',
+    link: '/certificate-3'
+  },
+  {
+    image: '/certificates/certificate-4.png',
+    title: 'Certificate 4',
+    link: '/certificate-4'
+  },
+  {
+    image: '/certificates/certificate-5.png',
+    title: 'Certificate 5',
+    link: '/certificate-5'
+  },
+  {
+    image: '/certificates/certificate-6.png',
+    title: 'Certificate 6',
+    link: '/certificate-6'
+  },
+  {
+    image: '/certificates/certificate-7.png',
+    title: 'Certificate 7',
+    link: '/certificate-7'
+  },
+  {
+    image: '/certificates/certificate-8.png',
+    title: 'Certificate 8',
+    link: '/certificate-8'
+  },
+  {
+    image: '/certificates/certificate-9.png',
+    title: 'Certificate 9',
+    link: '/certificate-9'
+  },
+  {
+    image: '/certificates/certificate-10.png',
+    title: 'Certificate 10',
+    link: '/certificate-10'
+  }
+]
