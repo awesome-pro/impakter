e3939db3f593263f479fe52d7a1c23d47ed437aa
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-08 17:02:57 +0530

    look update

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,25 +4,25 @@ import Link from 'next/link'
 
 export default function Page() {
   return (
-    <div className='w-full min-h-screen flex flex-col items-center justify-between bg-black bg-[url("/images/signin.jpg")] bg-cover bg-center gap-0'>
+    <div className='w-full min-h-screen  bg-black bg-[url("/images/signin.jpg")] bg-cover bg-center gap-0'>
+       <div className='h-full flex flex-col items-center justify-between min-h-screen w-screen bg-black/60 backdrop-blur-sm'>
         <span className='w-full '>
-          <Link href='/'>
-            <Image src='/logo.png' width={300} height={80} alt='logo' className='hover:z-20'/>
-          </Link>  
-        </span>
-        <div className='w-screen flex'>
-        <span className='w-16'>
-        </span>
-          <SignUp />
+            <Link href='/'>
+              <Image src='/logo.png' width={300} height={80} alt='logo' className='hover:z-20'/>
+            </Link>  
+          </span>
+          <div className='w-screen flex items-center justify-around'>
+            <SignUp />
+          </div>
+          <span className='flex gap-2 text-slate-300 text-xs   mb-5'>
+            <Link href='/privacy' className='hover:underline hover:text-white'>
+              Privacy
+            </Link>
+            <Link href='/contact' className='hover:underline hover:text-white'>
+            Contact
+            </Link>
+          </span>
         </div>
-        <span className='flex gap-2 text-slate-300 text-xs   mb-5'>
-          <Link href='/privacy' className='hover:underline hover:text-white'>
-            Privacy
-          </Link>
-          <Link href='/contact' className='hover:underline hover:text-white'>
-           Contact
-          </Link>
-        </span>
     </div>
   )
 }
