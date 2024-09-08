245cfbcfa62471fcd8f2c52d4d1167d93af107d3
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-08 17:09:49 +0530

    company hero update

--- components/company-hero.tsx
+++ components/company-hero.tsx
@@ -1,16 +1,17 @@
 import { ArrowBigDown } from 'lucide-react'
 import React from 'react'
+import { Button } from './ui/button'
 
 function CompanyHero({name}: {name: string}) {
   return (
     <div className='w-screen object-cover min-h-[660px] flex flex-col items-end' style={{ backgroundImage: "url('/company.png')"}}>
-      <div className='min-w-1/2'>
-      <h1 className='text-white text-6xl font-bold mt-48'>
+      <div className='w-screen min-h-[660px] backdrop-blur-sm bg-black/40 flex flex-col items-center justify-center'>
+      <h1 className='text-white text-6xl font-bold '>
         Is {name} a sustainable company?
       </h1>
-      <h4 className='text-lg font-semibold text-white'>
-        Check ESG Results Below<ArrowBigDown />
-      </h4>
+      <Button className='text-white mt-10 hover:bg-primary/50' size={'lg'}>
+        Check Analytics Below
+      </Button>
       </div>
     </div>
   )
